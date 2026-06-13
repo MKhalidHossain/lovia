d404410e2a58602d73395cfa7ec51ac4873cba87
MKhalidHossain <compilefiller@gmail.com>
2026-06-13 12:41:43 +0600

    swich to live url

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -2,7 +2,7 @@ abstract final class Env {
 
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000',
+    defaultValue: 'https://lovia-backend-rvud.onrender.com',
     // defaultValue: 'http://localhost:3000',
   );
 
